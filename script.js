c84d81679861b73770ba303ee7584047b27cd0ac
Codingbot97 <132872429+Codingbot97@users.noreply.github.com>
2024-01-31 19:24:42 +0530

    files added

---
+++ script.js
@@ -0,0 +1,74 @@
+let searchForm=document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active')
+}
+
+let shoppingCart=document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active')
+}
+
+let loginForm=document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active')
+}
+let navbar=document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+   
+}
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active')
+}
+
+//  Initialize Swiper
+
+// const mySwiper = new Swiper ('.swiper-container', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+//   speed: 300,
+//  mousewheel: true,
+//  coverflowEffect: {
+//   rotate: 30,
+//   slideShadows: true
+// },
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// })
+   
+  
+
+
+
+
+
+  
